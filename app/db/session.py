a5045482d433dbd603f41d877dd7e02c9f03f92c
FelipeHardmann <fashardmann@gmail.com>
2025-05-05 22:53:21 -0300

    feat: connection to postgres

--- app/db/session.py
+++ app/db/session.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
 
 
-DATABASE_URL = 'sqlite:///.db.sqlite3'
+DATABASE_URL = os.getenv('db_uri')
 
 engine = create_engine(
     DATABASE_URL,
